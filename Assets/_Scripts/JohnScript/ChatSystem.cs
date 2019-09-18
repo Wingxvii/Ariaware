c0f7eb1d8109e27236783ff12f72cbfb19602b8f
John <john.wang@uoit.net>
2019-09-17 21:42:32 -0400

    error bug

---
+++ Assets/_Scripts/JohnScript/ChatSystem.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Text;
+using System;
+using System.Collections.Generic;
+
+public enum PacketType
+{
+    //initialization connection
+    ERROR = 0,
+    //single string
+    MESSAGE = 1,
+}
+
+public class ChatSystem : MonoBehaviour
+{
+    [DllImport("CNET.dll")]
+    static extern IntPtr CreateClient();                            //Creates a client
+    [DllImport("CNET.dll")]
+    static extern void DeleteClient(IntPtr client);                 //Destroys a client
+    [DllImport("CNET.dll")]
+    static extern void Connect(string str, IntPtr client);          //Connects to c++ Server
+    [DllImport("CNET.dll")]
+    static extern void SendMsg(string str, IntPtr client);          //Sends Message to all other clients
+    [DllImport("CNET.dll")]
+    static extern void StartUpdating(IntPtr client);                //Starts updating
+    [DllImport("CNET.dll")]
+    static extern void SetupPacketReception(Action<int, int, string> action); //recieve packets from server
+
+    public Button sendButton;
+    public Text userLog;
+    public InputField userInput;
+
+    private IntPtr Client;
+
+    public List<List<string>> messageQueue = new List<List<string>>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //client Init  
+        Client = CreateClient();
+        Connect("127.0.0.1", Client);
+        StartUpdating(Client);
+        SetupPacketReception(PacketRecieved);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSendMessage()
+    {
+        string message = userInput.text;
+        SendMsg(message, Client);
+    }
+
+    private void OnDestroy()
+    {
+        //clean up client
+        DeleteClient(Client);
+    }
+
+    private void ProcessMessage(List<string> parsedData) {
+        int sender = int.Parse(parsedData[0]);
+
+        //filter by sender
+        if (sender == 0)
+        {
+            string message = "";
+            for (int counter = 1; counter < parsedData.Count; counter++)
+            {
+                message = message + parsedData[counter];
+            }
+            userLog.text += "CONSOLE: " + message + "\n";
+        }
+        else
+        {
+            string message = "";
+            for (int counter = 1; counter < parsedData.Count; counter++)
+            {
+                message = message + parsedData[counter];
+            }
+            message = "Player " + parsedData[0] + ": ";
+            userLog.text = userLog.text + message + "\n";
+        }
+
+    }
+
+    //called on data recieve action, then process
+    private void PacketRecieved(int type, int sender, string data) {                ///does this get multithreaded because it gets invoked in a seperate thread???s
+        List<string> parsedData = tokenize(',', data);
+        //send to parsed data
+        parsedData.Insert(0, sender.ToString());
+
+        switch ((PacketType)type) {
+            case PacketType.ERROR:
+                Debug.Log("PACKET SEND ERROR");
+                break;
+            case PacketType.MESSAGE:
+                ProcessMessage(parsedData);
+                break;
+        }
+
+    }
+
+    //tokenizer migrated from c++
+    private List<string> tokenize(char token, string text)
+    {
+        List<string> temp = new List<string>();
+        int lastTokenLocation = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == token)
+            {
+                temp.Add(text.Substring(lastTokenLocation, i - lastTokenLocation));
+                lastTokenLocation = i + 1;
+            }
+        }
+        return temp;
+    }
+}
--- Assets/_Scripts/JohnScript/SampleText.cs
+++ Assets/_Scripts/JohnScript/SampleText.cs
@@ -2,22 +2,59 @@
 using UnityEngine.UI;
 using UnityEngine;
 using System.Runtime.InteropServices;
+using System.Text;
+using System;
+
 public class SampleText : MonoBehaviour
 {
     [DllImport("CNET.dll")]
     static extern int Add(int a, int b);
+    [DllImport("CNET.dll")]
+    static extern IntPtr CreateClient();      //Creates a client
+    [DllImport("CNET.dll")]
+    static extern void DeleteClient(IntPtr client);      //Destroys a client
+
+    [DllImport("CNET.dll")]
+    static extern void Connect(string str, IntPtr client);      //Connects to c++
+
+
+    [DllImport("CNET.dll")]
+    static extern void SendString(StringBuilder str, int len);      //this recieves from c++
+    [DllImport("CNET.dll")]
+    static extern void RecieveString(string str);   //this sends to c++
 
     Text text;
+    IntPtr ClientModule;
 
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponent<Text>();
+        text = this.GetComponent<Text>();
+        ClientModule = CreateClient();
+        Connect("127.0.0.1", ClientModule);
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = Add(4,2).ToString();
+
+
+     /*   
+        string x = "awfwv";
+        RecieveString(x);
+
+        StringBuilder str = new StringBuilder(256);
+        SendString(str, str.Capacity);
+
+        text.text = str.ToString();
+        //text.text = SendString(str);
+   
+    */
+
+    }
+
+    private void OnDestroy()
+    {
+        DeleteClient(ClientModule);
     }
 }
