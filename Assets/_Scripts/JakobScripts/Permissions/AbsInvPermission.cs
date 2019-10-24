2ee753ecbd3d10356bca887c4ae353931c2e2e40
Jakob Anderson <jakob.anderson@uoit.net>
2019-10-24 18:15:51 -0400

    The big un-fuck (hopefully)

--- Assets/_Scripts/JakobScripts/Permissions/AbsInvPermission.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AbsInvPermission<T, U, V, W> : AbstractPermission<T, U, V, W> where T : AbsInvPermission<T, U, V, W> where U : AbsInvCommand<U, T, W, V> where V : Inventory where W : Controller
-{
-
-}
