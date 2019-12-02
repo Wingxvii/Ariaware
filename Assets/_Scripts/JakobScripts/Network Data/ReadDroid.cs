7302bfd470e33a5193e7394dd26bd4d63a5e8044
Jakob Anderson <jakob.anderson@uoit.net>
2019-12-01 20:35:12 -0500

    Animations...ish

--- Assets/_Scripts/JakobScripts/Network Data/ReadDroid.cs
+++ Assets/_Scripts/JakobScripts/Network Data/ReadDroid.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(Body))]
 public class ReadDroid : ReadBase
 {
+    Vector3 pseudoVel = Vector3.zero;
     Body b;
+    public Animator anim;
 
+    float dt = 0;
     protected override bool CreateVars()
     {
         if (base.CreateVars())
         {
             AddThird();
+            AddUpdate();
 
             b = GetComponent<Body>();
+            anim = GetComponent<Animator>();
 
             return true;
         }
@@ -21,8 +26,16 @@ public class ReadDroid : ReadBase
         return false;
     }
 
+    protected override void UpdateObject()
+    {
+        //Debug.Log(b.Rb.velocity);
+        anim.SetFloat("Walk", Vector3.Dot(pseudoVel, transform.forward) / 10);
+        anim.SetFloat("Turn", Vector3.Dot(pseudoVel, transform.right) / 10);
+    }
+
     protected override void Third()
     {
+        dt += Time.fixedDeltaTime;
         if (NDM != null && NDM.Init())
         {
             int dID = b.ID - NET_PACKET.NetworkDataManager.FPSmax - 1;
@@ -31,8 +44,11 @@ public class ReadDroid : ReadBase
 
                 NET_PACKET.NetworkDataManager.ReadRTS.droidData[dID].flag = false;
 
+                pseudoVel = (NET_PACKET.NetworkDataManager.ReadRTS.droidData[dID].position - transform.position) / dt;
+
                 transform.position = NET_PACKET.NetworkDataManager.ReadRTS.droidData[dID].position;
                 transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, NET_PACKET.NetworkDataManager.ReadRTS.droidData[dID].Yrot, transform.rotation.eulerAngles.z);
+                dt = 0f;
             }
         }
     }
